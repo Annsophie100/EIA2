f83c135fda8e1f425cd0467b72c99d52be6692a1
Klaus Roesch <Annsophie@klauss-mbp.eduroam.hs-furtwangen.de>
2019-03-21 15:07:11 +0100

    hi

--- Aufgabe00/aufgabe00.js
+++ Aufgabe00/aufgabe00.js
@@ -3,7 +3,7 @@ function greetUser() {
     if (eingabe != null) {
         document.getElementById("HuHu").innerHTML =
             "Herzlich Wilkommen, " + eingabe + "! Ich wünsche dir einen schönen Tag";
-        console.log("Huhu,", eingabe, "! Ich wünsche dir einen schönen Tag");
+        console.log("Huhu,", + eingabe, + "! Ich wünsche dir einen schönen Tag");
     }
 }
 //# sourceMappingURL=aufgabe00.js.map
