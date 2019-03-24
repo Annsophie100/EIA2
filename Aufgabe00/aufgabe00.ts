62b97a54af068747eb59715a522f1726def8565d
Klaus Roesch <Annsophie@Klauss-MBP.fritz.box>
2019-03-24 15:22:40 +0100

    NewShit

--- Aufgabe00/aufgabe00.ts
+++ Aufgabe00/aufgabe00.ts
@@ -1,8 +1,8 @@
 function greetUser(): void {
-    var eingabe: string = prompt("Wie heißt du?");
+    let eingabe: string = prompt("Wie heißt du?");
     if (eingabe != null) {
         document.getElementById("HuHu").innerHTML =
             "Herzlich Wilkommen, " + eingabe + "! Ich wünsche dir einen schönen Tag";
-        console.log("Huhu,", eingabe, "! Ich wünsche dir einen schönen Tag");
+        console.log("Huhu," + eingabe + "! Ich wünsche dir einen schönen Tag");
     }
 }  
