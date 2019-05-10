e374d785dfc0055f2d1616f9bfccbd6d4a2c92fa
Klaus Roesch <Annsophie@Klauss-MBP.fritz.box>
2019-05-10 16:12:15 +0200

    new

--- Aufgabe07/Server/server.ts
+++ Aufgabe07/Server/server.ts
@@ -36,11 +36,8 @@ namespace L07_Server {
 
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write("<h4>folgende Bestellung ist auf dem Server eingegangen:</h4>");
 
         let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-
-
 
         for (let key in url.query) {
 
