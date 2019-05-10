bcf42888f25e118808d33dabdee230dee8e02c9e
Klaus Roesch <Annsophie@Klauss-MBP.fritz.box>
2019-05-10 15:00:57 +0200

    A07 up

---
+++ Aufgabe07/Server/server.ts
@@ -0,0 +1,43 @@
+/* 
+Aufgabe: 07
+Name: Annsophie Rösch
+Matrikel: 257727
+Datum: 12.05.2019
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. 
+*/
+
+// importiert Datei als http
+import * as Http from "http";
+import * as Url from "url";
+
+namespace A07_server {
+
+    console.log("Starting server");
+    let port: number = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+
+    let server: Http.Server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+
+    function handleListen(): void {
+        console.log("Server listening on port " + port);
+
+    }
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("Eiszzzzzzzeit!");
+
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+
+        
+        _response.write(_request.url);
+        console.log(_request.url);
+        _response.end();
+    }
+}
