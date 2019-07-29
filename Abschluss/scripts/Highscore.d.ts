4e3b8e60a7342adefccafdddc00b32a528dc8aaf
Annsophie100 <48793597+Annsophie100@users.noreply.github.com>
2019-07-29 09:21:04 +0200

    delete wrong file

--- Abschluss/scripts/Highscore.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace abschluss {
-    class Highscore extends Objects {
-        xPos: number;
-        yPos: number;
-        constructor();
-        update(): void;
-        draw(): void;
-    }
-}
